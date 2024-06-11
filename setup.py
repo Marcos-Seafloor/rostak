d67c1ab0de2bb00d4baf792a967a319b7710fc71
Marcos Barrera <marcos.barrera@seafloorsystems.com>
2024-06-11 11:24:13 -0700

    Refactor setup.py for rostak bridge
    - Update the package name to 'rostak'
    - Set the version to '1.0.0'
    - Specify the packages and data files
    - Add install_requires for setuptools
    - Set zip_safe to True
    - Assign maintainer as Marcos Barrera with email marcos@seafloor.com
    - Provide a description for the package as 'A rostak bridge for ROS2'
    - License is marked as TODO
    - Include tests_require with pytest
    - Define console_scripts entry points for 'rostak_bridge' and 'roscot_fix'
    
    This commit refactors the setup.py file for the rostak bridge, updating various configuration parameters such as package name, version, packages, data files, dependencies, maintainer information, description, license, tests_require, and entry points. These changes ensure that the setup.py file accurately reflects the requirements and specifications of the rostak bridge package.

--- setup.py
+++ setup.py
@@ -1,10 +1,27 @@
 from setuptools import setup
-from catkin_pkg.python_setup import generate_distutils_setup
 
-d = generate_distutils_setup(
-    packages=['rostak'],
-    scripts=['scripts/rostak_bridge', 'scripts/roscot_fix'],
-    package_dir={'': 'src'}
+package_name = 'rostak'
+
+setup(
+    name=package_name,
+    version='1.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Marcos Barrera',
+    maintainer_email='marcos@seafloor.com',
+    description='A rostak bridge for ROS2',
+    license='TODO',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'rostak_bridge = rostak.rostak_bridge:main',
+            'roscot_fix = rostak.roscot_fix:main',
+        ],
+    },
 )
-
-setup(**d)
