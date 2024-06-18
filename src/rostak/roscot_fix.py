657f01143b9b2a5df682a6e465ce22d78d13a034
Marcos Barrera <marcos.barrera@seafloorsystems.com>
2024-06-18 07:19:59 -0700

    Refactor code to update heading and ground speed from VFR_HUD message
    - Add import statement for `mavros_msgs.msg.VFR_HUD`
    - Create `update_heading_and_speed` method to update `heading` and `ground_speed` attributes
    - Subscribe to `/mavros/vfr_hud` topic and call `update_heading_and_speed` method
    - Modify `publish_fix` method to pass updated values of `heading` and `ground_speed` when calling `new_status_msg`
    
    These changes refactor the code in order to update the heading and ground speed values based on the VFR_HUD message. This allows for more accurate status COT event generation.

--- src/rostak/roscot_fix.py
+++ src/rostak/roscot_fix.py
@@ -2,6 +2,7 @@ import rospy
 from rostak.cot_utility import CotUtility
 from std_msgs.msg import String
 from sensor_msgs.msg import NavSatFix
+from mavros_msgs.msg import VFR_HUD
 
 class RosCotFix:
     def __init__(self):
@@ -12,13 +13,26 @@ class RosCotFix:
         self.tx = rospy.Publisher('tak_tx', String, queue_size=1)
         self.msg = String()
         rospy.Subscriber("fix", NavSatFix, self.publish_fix)
+        self.heading = 0
+        self.ground_speed = 0
+        rospy.Subscriber("/mavros/vfr_hud", VFR_HUD, self.update_vfr_hud)
         rospy.loginfo(self.util.get_config())
+
+    def update_heading_and_speed(self, msg):
+        """Update heading and ground speed from VFR_HUD message."""
+        self.heading = msg.heading
+        self.ground_speed = msg.groundspeed
 
     def publish_fix(self, msg):
         """Generate a status COT Event."""
-        self.util.set_point(msg)
+        #self.util.set_point(msg)
+        self.util.set_point({
+            "latitude": msg.latitude,
+            "longitude": msg.longitude,
+            "altitude": msg.altitude
+        })
         stale_in = 2 * max(1, 1 / self.rate)
-        self.msg.data = self.util.new_status_msg(stale_in)
+        self.msg.data = self.util.new_status_msg(stale_in, self.heading, self.ground_speed)
         self.tx.publish(self.msg)
 
 if __name__ == '__main__':
